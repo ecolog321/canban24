79cc240395532eff9c1888dba1501f582e7b66ef
ecolog321 <ecakrhipov@gmail.com>
2024-05-12 16:47:03 +0800

    trying do cards

--- src/components/Popups/PopupBrowse/PopupBrowse.jsx
+++ src/components/Popups/PopupBrowse/PopupBrowse.jsx
@@ -1,4 +1,3 @@
-
 import { Status } from "./Status/Status";
 import { ThemeCategories } from "./ThemeCategories/ThemeCategories";
 import { TopBlock } from "./TopBlock/TopBlock";
@@ -10,8 +9,20 @@ import {
   PopBrowseContent,
 } from "./PopupBrowse.styled";
 import { PopBtnBrowse } from "./PopBtnBrowse/PopBtnBrowse";
+import { useTasks } from "../../../context/hooks/useTasks";
 
 export const PopupBrowse = ({ cardID, $display }) => {
+  const { tasks } = useTasks();
+
+  const currentTask=tasks.map((task)=> { 
+    if (task._id===cardID) {
+      currentTask=task;
+      console.log(currentTask)
+    }
+   });
+
+
+
   return (
     <PopBrowse $display={$display}>
       <PopBrowseContainer>
